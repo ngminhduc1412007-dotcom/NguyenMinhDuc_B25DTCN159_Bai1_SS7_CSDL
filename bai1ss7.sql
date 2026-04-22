53cd91845cd43bf157993108797071a0e7cc6fc2
Nguyễn Minh Đức <ngminhduc1412007@gmail.com>
2026-04-22 14:39:44 +0700

    Add files via upload

---
+++ bai1ss7.sql
@@ -0,0 +1,14 @@
+-- khi price = (subquery) có nghĩa là cột price đấy được gán 1 giá trị cứng chứ không phải nhiều giá trị
+-- Hôm nay chạy được là do price chỉ có một giá trị và subquery chỉ trả về một giá trị 
+-- Hôm sau bị lỗi là do đã thêm nhiều khoá học khác khiến subquery trả về nhiều giá trị khác nhau
+-- khiến SQL không thể xử lý
+-- Chính điều đó khiến hệ thống bị lỗi và màn hình của kế toán hiển thị lỗi 
+
+-- Câu lệnh đúng
+SELECT title, price
+FROM Courses
+WHERE price IN (
+    SELECT price
+    FROM Courses
+    WHERE instructor_id = 5
+);
